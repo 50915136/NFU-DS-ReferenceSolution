7df4fe6ed481c3cb1984594c1f028cc50fec0db0
LT <a0919329@gmail.com>
2024-07-17 14:55:44 +0800

    Create classObjectCreate.cpp

---
+++ 0717/classObjectCreate.cpp
@@ -0,0 +1,24 @@
+// 一般類別物件的建立
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Account {
+   public:
+    string id;
+    string name;
+    double balance;
+};
+
+int main() {
+    Account acc;
+    acc1.id = "我愛";
+    acc1.name = "瑞克";
+    acc1.balance = 48;
+
+    Account acc2;
+    acc2.id = "福瑞";
+    acc2.name = "搖";
+    acc2.balance = 763;
+}
